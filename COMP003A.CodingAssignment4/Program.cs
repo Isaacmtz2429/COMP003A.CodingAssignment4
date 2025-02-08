009da46fd2889854267e7bb1e4e6dcd677448ae3
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-08 00:35:02 -0800

    case one done and forgot to add "enter your choice" part.

--- COMP003A.CodingAssignment4/Program.cs
+++ COMP003A.CodingAssignment4/Program.cs
@@ -11,20 +11,34 @@ namespace COMP003A.CodingAssignment4
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Inventory Management System!!");
-            Console.WriteLine("Data storage type: List"); // if you read this, sorry don't know how to set both options up
+            Console.WriteLine("Data storage type: List"); // if you read this, sorry don't know how to set both options
 
             List<string> productNames = new List<string>();
-            List<int> productQuantity = new List<int>();
+            List<int> productQuantities = new List<int>();
 
             while (true)
             {
-                Console.WriteLine("\nInventory Management System Menu::");
+                Console.WriteLine("\nInventory Management System Menu:");
                 Console.WriteLine("1. Add a product");
                 Console.WriteLine("2. Update Product Quantity");
                 Console.WriteLine("3. View Inventory Summary");
                 Console.WriteLine("4. Exit");
+                Console.Write("Enter your choice: ");
                 int choice = int.Parse(Console.ReadLine());
 
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Please Enter product name: ");
+                        string productName = Console.ReadLine();
+                        Console.Write("Now Please Enter product quantity: ");
+                        int productQuantity = int.Parse(Console.ReadLine());
+
+                        productNames.Add(productName);
+                        productQuantities.Add(productQuantity);
+                        Console.WriteLine("Product added successfully!");
+                        break;
+                }
             }
         }
     }
