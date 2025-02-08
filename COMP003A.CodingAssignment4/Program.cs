6af9df86110b5ec72df34a9c7c34d5c1aefe1f2c
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-08 00:16:42 -0800

    list string and int and meny system - set up

--- COMP003A.CodingAssignment4/Program.cs
+++ COMP003A.CodingAssignment4/Program.cs
@@ -10,7 +10,22 @@ namespace COMP003A.CodingAssignment4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Inventory Management System!!");
+            Console.WriteLine("Data storage type: List"); // if you read this, sorry don't know how to set both options up
+
+            List<string> productNames = new List<string>();
+            List<int> productQuantity = new List<int>();
+
+            while (true)
+            {
+                Console.WriteLine("\nInventory Management System Menu::");
+                Console.WriteLine("1. Add a product");
+                Console.WriteLine("2. Update Product Quantity");
+                Console.WriteLine("3. View Inventory Summary");
+                Console.WriteLine("4. Exit");
+                int choice = int.Parse(Console.ReadLine());
+
+            }
         }
     }
 }
